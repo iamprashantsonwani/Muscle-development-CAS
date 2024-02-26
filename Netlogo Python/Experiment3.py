2df79ec2a4bed996e413d6150d3f22ab972500cc
Prashant Sonwani <sonwani.prashant01@gmail.com>
2024-02-26 15:10:54 -0600

    Add files via upload
    Initial commit

---
+++ Netlogo Python/Experiment3.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import numpy as np
+
+import pandas as pd
+import matplotlib
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set_style('white')
+sns.set_context('talk')
+
+from scipy.stats import t
+import pyNetLogo
+
+
+netlogo = pyNetLogo.NetLogoLink(gui=True)
+
+netlogo.load_model('H:/ProjectCAS/Draft/Muscle_Development.nlogo')
+
+
+netlogo.command('setup')
+
+netlogo.command('set lift?'" "+str('true'))
+netlogo.command('set intensity'" "+str(50))
+
+netlogo.command('set hours-of-sleep'" "+str(10))
+netlogo.command('set days-between-workouts'" "+str(4))
+netlogo.command('set %-slow-twitch-fibers'" "+str(60))
+
+netlogo.command('set inconsistency?'" "+str('true'))
+netlogo.command('set inconsistencies-days-sleep'" "+str(40))
+
+netlogo.command('setup')
+
+musclemass = [] 
+anabolicstat = []
+catabolicstat = []
+no_of_ticks = []
+
+for i in range(3500): 
+    try: 
+        netlogo.command('go') 
+        x1 = netlogo.report('muscle-mass') 
+        musclemass.append(x1/100)
+
+        y1 = netlogo.report('mean [catabolic-hormone] of patches')
+        catabolicstat.append(y1)
+
+        z1 = netlogo.report('mean [anabolic-hormone] of patches')
+        anabolicstat.append(z1)
+
+    except:
+        x1 = 0
+        musclemass.append(x1)
+        y1 = 0
+        anabolicstat.append(y1)
+        z1 = 0
+        catabolicstat.append(z1)
+        
+    no_of_ticks.append(i+1)
+plt.plot(no_of_ticks,musclemass) 
+plt.ylabel("Muscle mass")
+plt.xlabel("No of Ticks") 
+plt.title("Muscle development") 
+plt.show()
+
+plt.plot(no_of_ticks,catabolicstat) 
+plt.ylabel("catabolic harmone")
+plt.xlabel("No of Ticks") 
+plt.title("catabolic") 
+plt.show()
+
+plt.plot(no_of_ticks,anabolicstat) 
+plt.ylabel("anabolic harmone")
+plt.xlabel("No of Ticks") 
+plt.title("anabolic") 
+plt.show()
+
+#netlogo.kill_workspace()
